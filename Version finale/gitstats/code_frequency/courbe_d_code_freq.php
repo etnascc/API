48f1568d29c743b052baef0ec44ad0348add9d7b
Nesrine Ould <nesrine1092@gmail.com>
2015-05-21 15:29:02 +0200

    Update courbe_d_code_freq.php
    delete des lignes de codes inutiles

--- Version finale/gitstats/code_frequency/courbe_d_code_freq.php
+++ Version finale/gitstats/code_frequency/courbe_d_code_freq.php
@@ -6,8 +6,6 @@ require_once("../jpgraph/src/jpgraph_line.php");
 require_once("../jpgraph/src/jpgraph_bar.php");
 include ("../betav3.php");
 
-// $donnees = array(12,23,9,58,23,26,57,48,12);
-#modif
 $parsed_json = json_decode(my_get_json("rate_limit?access_token=a6f162fe9dd5745cfaa1e387321b3ce59ede3a27"),true);
 	if ($parsed_json['rate']['remaining'] != 0)
 	{
@@ -37,11 +35,6 @@ $graphe->xaxis->SetTickLabels(array('1'));
 $histo = new BarPlot($donnees);
 // Ajout de l'histogramme au graphique
 $graphe->add($histo);
-
-// Creation de la courbe
-//$courbe = new LinePlot($donnees);
-// Ajout de la courbe au graphique
-//$graphe->add($courbe);
 
 // Ajout du titre du graphique
 $graphe->title->set("Le nombre de deletion par jour sur un an : ");
